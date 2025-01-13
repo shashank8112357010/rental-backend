839ce5ec097bb7ec8f1b2ecd30a7a1e264231dfb
shashank8112357010 <shashanksharma1235999@gmail.com>
2025-01-13 13:34:32 +0530

    removed user

--- validators/bookingValidator.js
+++ validators/bookingValidator.js
@@ -1,7 +1,7 @@
 import Joi from 'joi';
 
 const bookingSchema = Joi.object({
-  user: Joi.string().hex().length(24).required(), // ObjectId is a 24-character hexadecimal string
+ 
   itemType: Joi.string().valid('Property', 'Vehicle').required(),
   item: Joi.string().hex().length(24).required(), // ObjectId reference
   status: Joi.string().valid('PENDING', 'APPROVED', 'REJECTED').default('PENDING'),
